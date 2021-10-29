e71603555b14a35031fac1a6c3d2839cb349cdab
aloysioy <aloysioy@yahoo.com.br>
2021-10-29 14:34:22 +0000

    Incluido menu e cadastro de aluno e professor.

--- src/main/Programa.java
+++ src/main/Programa.java
@@ -34,49 +34,6 @@ public class Programa {
 			}
 	
 		}
-		/*
-		AlunoService a = new AlunoService();
-
-		// Incluir Alunos
-		System.out.println("Incluindo Alunos");
-		try {
-			a.salvarAluno("Aluno 1","111.111.111-11","Sobrenome 1","11","Analise",2021);
-		} catch (Exception $e) {
-			System.out.println($e.getMessage());
-		}
-		a.salvarAluno("Aluno2","222.222.222-22","Sobrenome 2","2222","Banco de Dados",2021);
-
-		ProfessorService p = new ProfessorService();
-
-		// Incluir Professor
-		System.out.println("\nIncluindo Professores");
-		try {
-			p.salvarProfessor("1","Professor 1","33.333.333-33","Sobrenome 1","POO",2015);
-		} catch (Exception $e) {
-			System.out.println($e.getMessage());
-		}
-		try {
-			p.salvarProfessor("2","Professor 2","44.444.444-44","Sobrenome 2","Banco de Dados",2013);
-		} catch (Exception $e) {
-			System.out.println($e.getMessage());
-		}
-
-		FornecedorService f = new FornecedorService();
-
-		// Incluindo Fornecedores
-		System.out.println("\nIncluindo Fornecedores");
-		try {
-			f.salvarFornecedor("Fornecedor 1", "Fornecedor Fantasia 1", "11.111.111/1111-11", "1", "Construção", "01/01/2020", "Contato 1", "email1@localhost.com", "(11) 1111-1111");
-		} catch (Exception $e) {
-			System.out.println($e.getMessage());
-		}
-		try {
-			f.salvarFornecedor("Fornecedor 2", "Fornecedor Fantasia 2", "22.222.222/2222-22", "2", "Informatica", "01/01/20", "Contato 2", "email2@localhost.com", "(22) 2222-2222");
-		} catch (Exception $e) {
-			System.out.println($e.getMessage());
-		}
-
-		*/
 	}
 
 	private static void menuPrincipal() {
@@ -132,7 +89,7 @@ public class Programa {
 	
 			switch(opcao) {
 				case 1:
-					//MenuCadastros.iniciar("Professor");
+					incluirFornecedor();
 					break;
 				case 2:
 					System.out.println("\nListando Fornecedores");
@@ -150,6 +107,43 @@ public class Programa {
 					opcaoErrada();
 					break;
 			}
+		}
+	}
+
+	private static void incluirFornecedor() {
+		String codFornecedor = "";
+		String nomefantasia = "";
+		String razaosocial = "";
+		String segmento = "";
+		String cnpj = "";
+		String dataCadastro = "";
+		String contato = "";
+		String email = "";
+		String telefone = "";
+		System.out.println("Digite o codigo do fornecedor:");
+		codFornecedor = teclado.nextLine();
+		System.out.println("Digite o nome fantasia do fornecedor:");
+		nomefantasia = teclado.nextLine();
+		System.out.println("Digite oa razão social do fornecedor:");
+		razaosocial = teclado.nextLine();
+		System.out.println("Digite o cnpj do fornecedor:");
+		cnpj = teclado.nextLine();
+		System.out.println("Digite o segmento do fornecedor:");
+		segmento = teclado.nextLine();
+		System.out.println("Digite a data do cadastro do fornecedor:");
+		dataCadastro = teclado.nextLine();
+		System.out.println("Digite o contato do fornecedor:");
+		contato = teclado.nextLine();
+		System.out.println("Digite o telefone do fornecedor:");
+		telefone = teclado.nextLine();
+		System.out.println("Digite o e-mail do fornecedor:");
+		email = teclado.nextLine();
+		FornecedorService f= new FornecedorService();
+		try {
+			f.salvarFornecedor(nomefantasia, razaosocial, cnpj, codFornecedor, segmento, dataCadastro, contato, email, telefone);
+		} catch (Exception $e) {
+			System.out.println("\n*****************\nCadastro com Erro\n*****************");
+			System.out.println($e.getMessage());
 		}
 	}
 
@@ -225,7 +219,7 @@ public class Programa {
 	
 			switch(opcao) {
 				case 1:
-					//MenuCadastros.iniciar("Professor");
+					incluirAluno();;
 					break;
 				case 2:
 					System.out.println("\nListando Alunos");
@@ -242,6 +236,37 @@ public class Programa {
 					opcaoErrada();
 					break;
 			}
+		}
+	}
+
+	private static void incluirAluno() {
+		String matricula = "";
+		String nome = "";
+		String sobrenome = "";
+		String cpf = "";
+		String anoDoIngresso = "";
+		String turma = "";
+		System.out.println("Digite a matrícula do aluno:");
+		matricula = teclado.nextLine();
+		System.out.println("Digite o nome do aluno:");
+		nome = teclado.nextLine();
+		System.out.println("Digite o sobrenome do aluno:");
+		sobrenome = teclado.nextLine();
+		System.out.println("Digite o cpf do aluno:");
+		cpf = teclado.nextLine();
+		System.out.println("Digite a turma do aluno:");
+		turma = teclado.nextLine();
+		System.out.println("Digite o ano do ingresso:");
+		anoDoIngresso = teclado.nextLine();
+		if (!anoDoIngresso.matches("[0-9.]+")) {
+			anoDoIngresso = "0";
+		}
+		AlunoService a = new AlunoService();
+		try {
+			a.salvarAluno(nome,cpf,sobrenome,matricula,turma,Integer.parseInt(anoDoIngresso));
+		} catch (Exception $e) {
+			System.out.println("\n*****************\nCadastro com Erro\n*****************");
+			System.out.println($e.getMessage());
 		}
 	}
 }
